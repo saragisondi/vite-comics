e20f148fd2f1bd6da1c8806d63a3e4e6f010adc2
Sara Gisondi <sara.gisondi@outlook.it>
2023-04-27 16:55:22 +0200

    add mixin d-flex

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,22 +13,39 @@ export default {
 </script>
 
 <template>
-  <div>
-    <img src="../assets/img/dc-logo.png" alt="logo">
-  </div>
-  <ul>
-    <li v-for="(link, index) in headerMenu" :key="index">
-      <a href="#">{{link.text}}</a>
-    </li>
-  </ul>
+
+  <header>
+    <div class="container-img">
+      <img src="../assets/img/dc-logo.png" alt="logo">
+    </div>
+  
+    <ul>
+      <li v-for="(link, index) in headerMenu" :key="index">
+        <a href="#">{{link.text}}</a>
+      </li>
+    </ul>
+  </header>
+
 </template>
 
 <style lang="scss" scoped>
+  @use '../scss/general.scss' as *;
   @use '../scss/colors.scss' as *;
+
+  header{
+    @include d-flex;
+  }
 
   a{
     color:$primary-color;
   }
 
+  li{
+    background-color:red
+  }
+
+  .container-img{
+    background-color:black
+  }
 
 </style>
